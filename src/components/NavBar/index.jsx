d2b75f55c5ec710dad9b7a156112fc90988952dc
matiasdemauro <matiasdemauro@gmail.com>
2022-08-30 10:56:50 -0300

    añado evento para cambiar color de menu

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,38 +1,47 @@
 import React from 'react'
 import CartWidget from '../CartWidget';
+import Select from '../Select';
 import './style.css'
+import { useState } from 'react'
 
 
-
-const NavBar = ({categoria , temp}) => {
- console.log(categoria)
- console.log(temp);
- 
+const NavBar = () => {
+ //Nombre del estado, setNombreDelEstado
+ const [navColor,setNavColor] = useState("#f3f3f3")
+ const onChangeColor = (event) =>{
+  //console.log(event);
+ // console.log(event.target.value)
+ const color = event.target.value;
+  setNavColor(color);
+ }
+ console.log(navColor);
     return (
-      <>{/* <div className = 'cajaSup'>
-        <p>categorias : {categoria}</p>
-        <p>temp °C : {temp}</p>
-        </div>*/}
+      <>
     <div className='nav'>
     
 
-    <ul className='menu'> 
-    <a href='#'>  <li className='item'>Gianne-app</li> </a>
-    <a href='#'>   <li className='item'>
+    <ul style={{
+       backgroundColor : navColor
+    }} className='menu'
+    > 
+    <a href='/#'>  <li className='item'>Gianne-app</li> </a>
+    <a href='/#'>   <li className='item'>
               Productos
               <ul className='submenu'>
-                 <a href='#'> <li className='subitem'>Sillones</li></a>
-                 <a href='#'> <li className='subitem'>Mesas</li></a>
-                 <a href='#'> <li className='subitem'>Bazar</li></a>
-                 <a href='#'> <li className='subitem'>Deco</li></a>
+                 <a href='/#'> <li className='subitem'>Sillones</li></a>
+                 <a href='/#'> <li className='subitem'>Mesas</li></a>
+                 <a href='/#'> <li className='subitem'>Bazar</li></a>
+                 <a href='/#'> <li className='subitem'>Deco</li></a>
               </ul>
         </li> </a>
-        <a href='#'>   <li className='item'>Nosotros</li> </a>
-        <a href='#'>   <li className='item'>Contacto</li> </a>
+        <a href='/#'>   <li className='item'>Nosotros</li> </a>
+        <a href='/#'>   <li className='item'>Contacto</li> </a>
+        <CartWidget/>
+        <Select handleColor ={onChangeColor}/>
   </ul>
       
   </div>
-  <CartWidget/>
+  
     </>
   );
 }
