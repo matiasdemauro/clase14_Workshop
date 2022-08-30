ebdfa1f9b81da69ae84e65505fb47168658efc15
matiasdemauro <matiasdemauro@gmail.com>
2022-08-30 12:09:56 -0300

    se agregan funciones

---
+++ src/components/ItemCount/index.jsx
@@ -0,0 +1,40 @@
+import React , {useState , useEffect} from 'react'
+import './style.css'
+
+const ItemCount = ({stock , initial , onAdd}) => {
+  
+  const [count, setCount] = useState(initial);
+    const handleAdd = () =>{
+        if(count < stock) {
+            setCount (count+1);
+        } else{
+            alert('No hay suficiente stock disponible');}
+
+    }
+    const handleDecrement = () => {
+       // desarrollo del decremen
+    }
+ 
+    
+    //ciclo de vida
+    useEffect (()=> {
+        //el array de dependencias vacio implica que el callback se ejecutrara cuando e MONTA el componente por UNICA vez.
+        console.log('se monto el ItemCount');
+    } , []);
+    
+    //La funciion callback dentro del useEffect se ejecutara cuando se MONTE el componente, y cuando se ACTUALICE el valor del count.
+    useEffect (()=>{
+        console.log('Se actualiza el estado');
+    }, [count]);
+
+    return (
+    <div>
+        <button onClick= {handleDecrement}> - </button>
+        <h2>{count}</h2>
+        <button onClick= {handleAdd}> + </button>
+        <button onClick={() => onAdd(count)}> Agregar al carrito </button>
+    </div>
+  )
+}
+
+export default ItemCount
--- src/container/ItemListContainer/index.jsx
+++ src/container/ItemListContainer/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
-
+import ItemCount from '../../components/ItemCount';
 import './style.css';
+
 const ItemListContainer = ({greeting}) => {
+  const agregarAlCarrito = (stock) => {
+    console.log(`Se agrego la cantidad ${stock} al carrito`);
+  }
   return (
     <div className='list-style-container'>
-        
-
-        <h1>{greeting}</h1>
+      <h1>{greeting}</h1>
+      <ItemCount initial={1} stock={5} onAdd={agregarAlCarrito}/>
     </div>
   )
 }
