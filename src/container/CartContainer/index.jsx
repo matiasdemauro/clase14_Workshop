b9b6359130ab59e426f8bd9c47621cecb3692e09
matiasdemauro <matiasdemauro@gmail.com>
2022-09-29 15:09:54 -0300

    guardarOrder usando Batch

--- src/container/CartContainer/index.jsx
+++ src/container/CartContainer/index.jsx
@@ -2,14 +2,18 @@ import React from 'react'
 import { useContext } from 'react'
 import { Shop } from './../../context/ShopProvider';
 import { DataGrid } from '@mui/x-data-grid';
-import { Button } from '@mui/material';
+import { Button, CircularProgress } from '@mui/material';
 import ordenGenerada from '../../services/generarOrden';
-
+import { collection, addDoc } from "firebase/firestore";
+//import { doc, updateDoc, getDoc } from "firebase/firestore";
+import { db } from './../../firebase/config';
+import { useState } from 'react';
+import guardarOrden from './../../services/guardarOrden';
 
 const Cart = () => {
   //Traigo los params de ShopProvider a Cart()
   const {cart , removeItem , clearCart , total} = useContext(Shop)
-  
+  const [loading, setLoading] = useState(false);
   console.log(cart);
   
   
@@ -35,10 +39,34 @@ const Cart = () => {
       </Button>
     );
   };
-  const handleBuy = () =>{
+  const handleBuy = async () =>{
+    setLoading(true);
     const importeTotal = total(cart);
-    const orden = ordenGenerada('Matias', 'matias@gmail.com' , '3254124' ,  cart, importeTotal )
+    const orden = ordenGenerada('Matias', 'matias@gmail.com' , '3254124' ,  cart, importeTotal );
     console.log(orden);
+
+
+
+
+      // Genero un nuevo documento en mi base de datos llamada orders utilizando el codigo proporcionado en firebase
+      const docRef = await addDoc(collection(db, "orders"), orden); 
+      //Actualizamos el stock del producto
+      guardarOrden(cart,orden);
+//      cart.forEach(async(productoEnCarrito)=>{
+//      const productRef = doc(db, "products", productoEnCarrito.id);
+//      //Llamamos al snapshot, llamando a firebase
+//      const productSnap = await getDoc(productRef);
+//      // snapshot.data() nos devuelve la informacion del documento a actualizar
+//      await updateDoc(productRef, {
+//        stock: productSnap.data().stock - productoEnCarrito.quantity,
+//      });
+//
+//    })
+    
+    // Set the "capital" field of the city 'DC'
+    setLoading(false);
+          alert(' Gracias por su compra, Orden generada con ID: ' + docRef.id);
+  
   }
  
   
@@ -64,7 +92,7 @@ const Cart = () => {
           });
         });
 
-        const rows = [];
+
   
   
     return (
@@ -78,7 +106,22 @@ const Cart = () => {
           rowHeight={150}
         />
         <Button onClick={clearCart} color='error' variant='outlined'>Clear cart</Button>
+        {loading ?  (    <div
+                        style={{
+                            display: "flex",
+                            flexDirection: "column",
+                            justifyContent: "center",
+                            width: "100%",
+                            height: "100%",
+                            alignItems: "center",
+                        }}
+                    >
+                        <CircularProgress/>
+                    </div>)
+                    :
+
         <Button onClick={handleBuy} >Confirmar Compra</Button>
+                    } 
       </div>
     );
     };
